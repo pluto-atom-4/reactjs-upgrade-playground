e49751caae46f86a06e83ae2b7cb88202262b80a
Akai Kaede <genko2@hotmail.com>
2025-11-13 09:38:41 -0800

    chore: update Prisma and related dependencies to version 6.19.0

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,16 +2,11 @@ import { fileURLToPath } from 'url';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 const srcPath = fileURLToPath(new URL('./src/', import.meta.url));
-
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
     exclude: [...configDefaults.exclude, '**/playwright/**'],
-    alias: {
-      '~': srcPath,
-      '~/': srcPath,
-    },
+    environment: 'jsdom',
     setupFiles: ['dotenv/config', '@testing-library/jest-dom'],
   },
   resolve: {
